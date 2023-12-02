2c93dbe0ebde2c7144c6790717fb5f45348ac1fe
seanhong0416 <106743783+seanhong0416@users.noreply.github.com>
2023-12-02 10:03:10 +0800

    working on defining databases

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,22 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
+
+class AuctionList(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    starting_bid = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_selling_items")
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_bought_items")
+
+class Bids(models.Model):
+    item = models.ForeignKey(AuctionList, on_delete=models.CASCADE, related_name="bids")
+    price = models.IntegerField()
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_bidding_items")
+
+class Comments(models.Model):
+    item = models.ForeignKey(AuctionList, on_delete=models.CASCADE, related_name="comments")
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="my_comments")
+    content = models.CharField(max_length=128)
